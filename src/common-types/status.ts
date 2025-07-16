51658456fd359285213fd75accb792de56204b57
leeyang-mac <leeyang031110@gmail.com>
2025-07-17 02:46:15 +0800

    Add additional response statuses to ResponseStatus enum

--- src/common-types/status.ts
+++ src/common-types/status.ts
@@ -1,5 +1,9 @@
 // enum to represent status from rpc
 export enum ResponseStatus {
     SUCCESS = "SUCCESS",
-    ERROR = "ERROR"
+    ERROR = "ERROR",
+    NOT_FOUND = "NOT_FOUND",
+    UNAUTHORIZED = "UNAUTHORIZED",
+    FORBIDDEN = "FORBIDDEN",
+    BAD_REQUEST = "BAD_REQUEST",
 }
